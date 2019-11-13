009a82629603387e5803a7cabfbcd3b0998f419a
marmai-jma <johane.marmai@gmail.com>
2019-11-13 10:14:02 +0100

    Week 3 - Tests junit FooBarQix - avec methode divisible + concat

--- src/main/java/foobarqix/FooBar.java
+++ src/main/java/foobarqix/FooBar.java
@@ -7,30 +7,43 @@ public class FooBar {
             throw new IllegalArgumentException("Les nombres négatifs ne sont pas acceptés");
         }
 
-        return divisible(input);
-    }
+        return divisible(input)+concat(input);
+}
 
     String divisible(int input){
         String result = "";
-        Boolean diviseurTrouve = false;
 
         if (input % 3 == 0) {
             result = result + "Foo";
-            diviseurTrouve = true;
         }
 
         if (input % 5 == 0) {
             result = result + "Bar";
-            diviseurTrouve = true;
         }
 
         if (input % 7 == 0) {
             result = result + "Qix";
-            diviseurTrouve = true;
         }
 
-        if (!diviseurTrouve) {
+        if (result =="") {
             result = "" + input;
+        }
+        return result;
+    }
+
+    String concat (int input){
+        String chaine = ""+input;
+        String result = "";
+        for (int i=0 ; i<chaine.length(); i++){
+            if (chaine.charAt(i) == '3'){
+                result = result + "Foo";
+            }
+            if (chaine.charAt(i) == '5'){
+                result = result + "Bar";
+            }
+            if (chaine.charAt(i) == '7'){
+                result = result + "Qix";
+            }
         }
         return result;
     }
--- src/test/java/foobarqix/FooBarQixTest.java
+++ src/test/java/foobarqix/FooBarQixTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class FooBarQixTest {
     FooBar fooBar = new FooBar();
 
+    // test d'exception inférieur ou égal à zéro
     @ParameterizedTest
     @ValueSource(ints ={-1,-2,-400})
     void should_refuse_negative_numbers(int input) {
@@ -15,10 +16,11 @@ public class FooBarQixTest {
                 () -> fooBar.fooBarQix(input));
     }
 
+    // tests de fooBar divisible
     @ParameterizedTest
     @ValueSource(ints = {1,2,4})
     void should_return_self_by_default(int input) {
-        String result = fooBar.fooBarQix(input);
+        String result = fooBar.divisible(input);
         Assertions.assertEquals(""+input, result);
     }
 
@@ -29,39 +31,109 @@ public class FooBarQixTest {
         Assertions.assertEquals(""+input, result);
     }
 
-
     @ParameterizedTest
     @ValueSource(ints = {3,6,9})
     void modulo_3_not_5_should_return_foo(int input) {
-        String result = fooBar.fooBarQix(input);
+        String result = fooBar.divisible(input);
         Assertions.assertEquals("Foo", result);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {5,10,25})
     void modulo_5_not_3_should_return_bar(int input) {
-        String result = fooBar.fooBarQix(input);
+        String result = fooBar.divisible(input);
         Assertions.assertEquals("Bar",result);
     }
 
     @ParameterizedTest
     @ValueSource(ints ={15,30,45})
     void modulo_3_and_5_should_return_foobar(int input) {
-        String result = fooBar.fooBarQix(input);
+        String result = fooBar.divisible(input);
         Assertions.assertEquals("FooBar",result);
     }
 
     @ParameterizedTest
     @ValueSource(ints ={7,49,14})
     void modulo_7_not_3_not_5_should_return_qix(int input) {
-        String result = fooBar.fooBarQix(input);
+        String result = fooBar.divisible(input);
         Assertions.assertEquals("Qix",result);
     }
 
     @ParameterizedTest
     @ValueSource(ints ={21})
     void modulo_7_3_should_return_fooqix(int input) {
-        String result = fooBar.fooBarQix(input);
+        String result = fooBar.divisible(input);
         Assertions.assertEquals("FooQix",result);
+    }
+
+    // tests de fooBar concat
+    @ParameterizedTest
+    @ValueSource(ints ={1,2,4})
+    void concat_default_return_empty_string (int input){
+        String result = fooBar.concat(input);
+        Assertions.assertEquals("",result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints ={3,31,13})
+    void concat_contains_3_return_foo_string (int input){
+        String result = fooBar.concat(input);
+        Assertions.assertEquals("Foo",result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints ={5,51,15})
+    void concat_contains_5_return_bar_string (int input){
+        String result = fooBar.concat(input);
+        Assertions.assertEquals("Bar",result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints ={7,71,17})
+    void concat_contains_7_return_bar_string (int input){
+        String result = fooBar.concat(input);
+        Assertions.assertEquals("Qix",result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints ={357,325471,305017})
+    void concat_contains_357_return_bar_string (int input){
+        String result = fooBar.concat(input);
+        Assertions.assertEquals("FooBarQix",result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints ={333,323431,303013})
+    void concat_contains_333_return_bar_string (int input){
+        String result = fooBar.concat(input);
+        Assertions.assertEquals("FooFooFoo",result);
+    }
+    // integration des 2 méthodes
+    @ParameterizedTest
+    @ValueSource(ints ={1,2,4})
+    void foobarqix_default_return_string_input (int input){
+        String result = fooBar.fooBarQix(input);
+        Assertions.assertEquals(""+input,result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints ={357})
+    void setFooBar_contains_div_3_div_7_357_return_bar_string (int input){
+        String result = fooBar.fooBarQix(input);
+        Assertions.assertEquals("FooQixFooBarQix",result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints ={31, 34,13})
+    void setFooBar_contains_3_return_input_foos_tring (int input){
+        String result = fooBar.fooBarQix(input);
+        Assertions.assertEquals(input+"Foo",result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints ={105})
+    void setFooBar_contains_5_div357_return_input_foobarqixbar_string (int input){
+        String result = fooBar.fooBarQix(input);
+        Assertions.assertEquals("FooBarQixBar",result);
     }
 }
